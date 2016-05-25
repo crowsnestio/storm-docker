46b2c1258c376bc210d77243620ffafd1654f65f
joshua sorenson <josh@crowsnest.io>
2016-05-24 18:17:40 -0700

    Adding optional pacemaker support

--- base/configure.sh
+++ base/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export LOCAL_IP=$(ifconfig eth0 | grep inet\ addr | awk '{print $2}' | cut -d: -f2)
+
 cat >> conf/storm.yaml <<EOF
 storm.local.dir: "/tmp"
 storm.local.hostname: "$LOCAL_IP"
@@ -15,6 +16,14 @@ fi
 if [ -n "$2" ]; then
    cat >> conf/storm.yaml <<EOF
 nimbus.seeds: ["$2"]
+EOF
+fi
+
+if [ -n "$PACEMAKER_SERVICE_HOST" ]; then
+	cat >> conf/storm.yaml <<EOF
+pacemaker.host: "$PACEMAKER_SERVICE_HOST"
+pacemaker.port: "$PACEMAKER_SERVICE_PORT"
+storm.cluster.state.store: "org.apache.storm.pacemaker.pacemaker_state_factory"
 EOF
 fi
 
