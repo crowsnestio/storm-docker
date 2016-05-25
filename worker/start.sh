5be81e02906347886e41d4c8425161455cbb30ec
joshua sorenson <josh@crowsnest.io>
2016-05-25 10:36:44 -0700

    Move the hostname override into just the worker start script

--- worker/start.sh
+++ worker/start.sh
@@ -2,4 +2,11 @@
 
 /configure.sh ${ZOOKEEPER_SERVICE_HOST:-$1} ${NIMBUS_SERVICE_HOST:-$2}
 
+# Make sure we override the worker local.hostname so workers can find each other easier.
+export LOCAL_IP=$(ifconfig eth0 | grep inet\ addr | awk '{print $2}' | cut -d: -f2)
+
+cat >> conf/storm.yaml <<EOF
+storm.local.hostname: "$LOCAL_IP"
+EOF
+
 exec /opt/apache-storm/bin/storm supervisor
