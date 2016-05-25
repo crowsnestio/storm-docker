9f3114c8ee92bf10eec12c12855145f86e920221
joshua sorenson <josh@crowsnest.io>
2016-05-25 10:15:24 -0700

    Add support for HA nimbus (look for _SECONDARY_HOST and _TERTIARY_HOST env vars)

--- base/configure.sh
+++ base/configure.sh
@@ -13,14 +13,31 @@ storm.zookeeper.servers: ["$1"]
 EOF
 fi
 
-if [ -n "$2" ]; then
+if [ -n "$NIMBUS_SERVICE_HOST" ]; then
    cat >> conf/storm.yaml <<EOF
-nimbus.seeds: ["$2"]
+nimbus.seeds:
+  - "$NIMBUS_SERVICE_HOST"
 EOF
 fi
 
+# If there are additional nimbus hosts, make sure to add them.
+if [ -n "$NIMBUS_SECONDARY_SERVICE_HOST" ]; then
+    cat >> conf/storm.yaml <<EOF
+  - "$NIMBUS_SECONDARY_SERVICE_HOST"
+EOF
+fi
+
+# If there are additional nimbus hosts, make sure to add them.
+if [ -n "$NIMBUS_TERTIARY_SERVICE_HOST" ]; then
+    cat >> conf/storm.yaml <<EOF
+  - "$NIMBUS_TERTIARY_SERVICE_HOST"
+EOF
+fi
+
+# If pacemaker is enabled, make sure to add that too.
+
 if [ -n "$PACEMAKER_SERVICE_HOST" ]; then
-	cat >> conf/storm.yaml <<EOF
+    cat >> conf/storm.yaml <<EOF
 pacemaker.host: "$PACEMAKER_SERVICE_HOST"
 pacemaker.port: $PACEMAKER_SERVICE_PORT
 storm.cluster.state.store: "org.apache.storm.pacemaker.pacemaker_state_factory"
